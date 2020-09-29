54f75bc69647b809a6b304b6dc8bc2602d3a29e0
Ben Bryant <brb182@txstate.edu>
2020-09-29 00:57:30 +0000

    fix4

--- hw4.cpp
+++ hw4.cpp
@@ -58,7 +58,8 @@ int product(list_t list)
 * if list is null return list
 * if rest is null return list
 * get first value of list
-* if rest is null
+* 
+* 
 * 
 */
 list_t reverse(list_t list)
@@ -79,29 +80,134 @@ list_t reverse(list_t list)
 
 /*
 * Pseudocode
-* if rest of list is not null
-* 
+* if rest of first list is not null
+* return append on rest of first list and list_make on first of first and second
 */
 list_t append(list_t first, list_t second)
 {
-  if(list_rest(first) == NULL)
-    return first;
+  if(list_rest(first) != NULL)
+    return append(list_rest(first), list_make(list_first(first), second));
 }
 
 /*
 * Pseudocode
-* get first value of list
-* if head % 2 == 0
-* 
+* if rest == null
+* return list
+* if first % 2 == 0
+* filter_odd on new list with last odd and list 
+* rest
+* else
+* set last_odd to list first
+* filter_odd on list rest
+* return list
 */
 list_t filter_odd(list_t list)
 {
-  int head_val = list_first(list);
-  if(head_val % 2 == 0)
+  int last_odd = 0;
+
+  if(list_rest(list) == NULL)
     return list;
+  
+  if(list_first(list) % 2 == 0)
+  {  
+    if(last_odd == 0)
+      filter_odd(list_make(last_odd, list_rest(list)));
+    else
+    {
+      last_odd = list_first(list);
+      filter_odd(list_rest(list));
+    }
+    return list;
+  }    
+      
+    
 }
 
+/*
+* Pseudocode
+* if rest == null
+* return list
+* if first % 2 == 0
+* filter_odd on new list with last odd and list 
+* rest
+* else
+* set last_odd to list first
+* filter_odd on list rest
+* return list
+*/
 list_t filter_even(list_t list)
 {
+  int last_even = 0;
 
+  if(list_rest(list) == NULL)
+    return list;
+  
+  if(list_first(list) % 2 != 0)
+  {  
+    if(last_even == 0)
+      filter_even(list_make(last_even, list_rest(list)));
+    else
+    {
+      last_even = list_first(list);
+      filter_even(list_rest(list));
+    }
+    return list;
+  }    
+}
+
+/*
+* Pseudocode
+* if fn(list_first) is false
+* return filter on new list with first element of 
+* rest and rest of rest
+* else return filter on list rest
+*/
+list_t filter(list_t list, bool (*fn)(int))
+{
+
+  if(fn(list_first(list) == false))
+    return filter(list_make(list_first(list_rest(list)), list_rest(list_rest(list))), fn);
+  else
+    return filter(list_rest(list), fn);
+}
+
+list_t rotate(list_t list, unsigned int n)
+{
+  if(n > 0)
+  {
+
+    rotate(list, n);
+  }
+}
+
+list_t insert_list(list_t first, list_t second, unsigned int n)
+{
+  
+}
+
+list_t chop(list_t l, unsigned int n)
+{
+
+}
+
+/*
+* Pseudocode
+* returns fib_tail(n)
+*/
+int fib(int n)
+{
+  return fib_tail(n);
+}
+
+/*
+* Pseudocode
+* if n <= 1
+* return n
+* return fib(n-1) + fib(n-2)
+*/
+int fib_tail(int n)
+{
+  if (n <= 1) 
+    return n; 
+  return fib(n-1) + fib(n-2); 
 }
